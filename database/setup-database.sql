958ec8e9ef04a53dae48009e7198d42581d55445
vbouault <vbouault@hotmail.fr>
2020-06-07 17:36:47 +0200

    ..

---
+++ database/setup-database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE express_post_put CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+USE express_post_put;
+
+CREATE TABLE IF NOT EXISTS user(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(64) UNIQUE NOT NULL,
+  password VARCHAR(64) NOT NULL,
+  name VARCHAR(128) NOT NULL
+);
